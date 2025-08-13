ff1fdb1b8c90307b8052ad1a9d7df4db26a6a44a
bharathii meena <24ucs118@kamarajengg.edu.in>
2025-08-13 12:35:19 +0530

    Add files via upload

---
+++ 4A.py
@@ -0,0 +1,29 @@
+class  Queue:
+    def __init__(self):
+        self.queue=[]
+    def is_empty(self):
+        return (self.queue)==0
+    def enqueue(self,item):
+        self.queue.append(item)
+    def dequeue(self):
+        if self.is_empty():
+            raise IndexError("Queue is empty")
+        return self.queue.pop(0)
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("Queue is empty")
+        return self.queue[0]
+    def size(self):
+        return len(self.queue)
+q=Queue()
+q.enqueue(10)
+q.enqueue(20)
+q.enqueue(30)
+print("Queue after enqueuing elements:",q.queue)
+print("Dequeue elements:",q.dequeue())
+print("Queue ater dequeuing an elemnt:",q.queue)
+print("front elements:",q.peek())
+print("queue size",q.size())
+
+
+            
